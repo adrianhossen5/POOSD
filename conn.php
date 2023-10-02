48de6fb26bc4dc701b1358f98889b47ecc0cdf01
Adrian Hossen <adrianhossen5@gmail.com>
2023-10-02 14:18:13 -0400

    Migrating API

--- conn.php
+++ conn.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "super";
-$password = "password";
-$dbname = "contact_manager";
+// load environment vars
+$servername = getenv('DB_SERVERNAME');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
