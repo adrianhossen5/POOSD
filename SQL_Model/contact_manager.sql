3de8eb66facf9566a7e82a80da602bcc8f2b0537
Adrian Hossen <adrianhossen5@gmail.com>
2023-10-08 18:48:32 -0400

    Using Ajax & Json

--- SQL_Model/contact_manager.sql
+++ SQL_Model/contact_manager.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 10, 2023 at 04:12 AM
+-- Generation Time: Oct 08, 2023 at 10:47 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `contacts` (
   `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `phone_number` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `user_id` varchar(36) COLLATE utf8mb4_general_ci NOT NULL,
+  `time_created` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -52,16 +53,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password_hash`, `email`) VALUES
-('9f41226e-4f8f-11ee-a707-a8a1594ea7f9', 'a', '$2y$10$sV3fSqG/D/Z5Mt55ZONOvOQobiphODMTBWvTFlqtBv1EA0slBF3D6', 'A@a.com'),
-('e95bd502-4f8f-11ee-a707-a8a1594ea7f9', 'c', '$2y$10$ynrIp12qfzjkS/eZ5MWSjuB5ATG15FnAkcnDTq/PVJlUgtCVr8F2.', 'c@c.com'),
-('ea8cd977-4f78-11ee-bb74-a8a1594ea7f9', 'b', '$2y$10$kxCNyyTlQXHvB9JxztuQjemGyddP2aRsNg/j/1VOxFIFZLjlUNd0u', 'b@b.com'),
-('ffe11a7e-4f8f-11ee-a707-a8a1594ea7f9', 'd', '$2y$10$.5S.WT5o1ukMD8cSM7sdA.cYLsDljr/RRe3ptxc1mqRQQbhMpR.sG', 'd@d.com');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
